146c862ccb29afe18f9a7312ac3aa9d1ea18c178
Ian Lawrence <ian@codezon.com>
2014-01-04 08:31:42 -0200

    Flushed out the admin

--- admin.py
+++ admin.py
@@ -1,7 +1,18 @@
 from django.contrib import admin
-from Karamoja.models import LivelihoodZones,Sectors, Indicators, Phases
+from Karamoja.models import LivelihoodZones, Map, Sectors, Indicators, Phase, Districts, RiskOfDrought, Trends, Years, Months, Report, Map, Documentation
 
 admin.site.register(LivelihoodZones)
 admin.site.register(Sectors)
 admin.site.register(Indicators)
-admin.site.register(Phases)
+admin.site.register(Phase)
+admin.site.register(Districts)
+admin.site.register(RiskOfDrought)
+admin.site.register(Trends)
+admin.site.register(Years)
+admin.site.register(Months)
+admin.site.register(Report)
+admin.site.register(Map)
+admin.site.register(Documentation)
+
+
+
